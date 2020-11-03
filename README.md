f4628573a723d919136bc072125aa7ae7ff04db5
Natalie Whitton <newhitton@gmail.com>
2020-11-03 17:03:07 +0100

    connected frontend to backend

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 [![Actions Status](https://github.com/naddiebell/MovieTheater-frontend/workflows/Movie_Theater_frontend/badge.svg)](https://github.com/naddiebell/MovieTheater-frontend/actions)
 
-
--- src/pages/BuyTickets/index.js
+++ src/pages/BuyTickets/index.js
@@ -1,29 +1,87 @@
-import React, { useState } from "react";
+/* eslint-disable func-names */
+/* eslint-disable no-console */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function BuyTickets() {
   const [displayTicket, setDisplayTicket] = useState(false);
- 
- function handleDisplay(e){
-   e.preventDefault();
-    setDisplayTicket(true);
- } 
- 
+  const [date, setDate] = useState("YYYY-MM-DD");
+  const [chooseFilm, setChooseFilm] = useState("-");
+  const [chooseTicket, setChooseTicket] = useState("");
+  // const [data, setData] = useState({
+  //   filmName: "",
+  //   dateOfFilm: "YYYY-MM-DD",
+  //   seatAmount: "",
+  // });
+
+  //const { filmName, dateOfFilm, seatAmount } = data;
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //   const result = await axios("/api/v1/tickets", {
+  //     manual: true,
+  //     data: { filmName, dateOfFilm, seatAmount },
+  //     method: "post",
+  //   });
+  //   setData(result.data);
+  // }, []
+  // );
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleFilmChange = (e) => {
+    setChooseFilm(e.target.value);
+  };
+
+  const handleTicketChange = (e) => {
+    setChooseTicket(e.target.value);
+  };
+
+  function handleDisplay(e) {
+    e.preventDefault();
+    console.log(date, chooseFilm, chooseTicket);
+    axios
+      .post("http://localhost:5709/api/v1/tickets", {
+        filmName: chooseFilm,
+        dateOfFilm: date,
+        seatAmount: chooseTicket,
+      })
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+    if (date !== "YYYY-MM-DD" && chooseFilm !== "-" && chooseTicket) {
+      setDisplayTicket(true);
+    }
+  }
+
   return (
     <>
-      <form
-      onSubmit={(e)=>handleDisplay(e)}
-      >
-        <label for="date">Välj ett datum:</label>
-        <input type="date" id="date" name="date"></input>
+      <form onSubmit={(e) => handleDisplay(e)}>
+        <label>Välj ett datum:</label>
+        <input type="date" value={date} onChange={handleDateChange}></input>
 
-        <label for="movies">Välj en film: </label>
-        <select id="movies" name="movies">
-          <option value="Default">-</option>
+        <label>Välj en film: </label>
+        <select value={chooseFilm} onChange={handleFilmChange} name="movies">
+          <option selected value="-">
+            -
+          </option>
           <option value="The-Princess-Bride">The Princess Bride</option>
           <option value="Pippi-Langstrumpor">Pippi Långstrumpor</option>
         </select>
-        <label for="quantity">Biljetter:</label>
+        <label>Biljetter:</label>
         <input
+          value={chooseTicket}
+          onChange={handleTicketChange}
           type="number"
           id="quantity"
           name="quantity"
@@ -31,9 +89,7 @@ export default function BuyTickets() {
           max="25"
         ></input>
 
-        <input type="submit"
-        
-        ></input>
+        <input type="submit"></input>
       </form>
       <p>Alla filmer börjar kl. 7</p>
       <p>Betalning är endast kontant vid dörren</p>
