9660bd5d49507b27c4acd8b099905edbbef3bb74
Natalie Whitton <newhitton@gmail.com>
2020-11-07 22:30:57 +0100

    select and deselect seats

--- src/Components/Seat/Seat.js
+++ src/Components/Seat/Seat.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import "./style.css";
 
-function Seat(props) {
-  const [click, setClick] = useState(null);
+function Seat({ value, onClick }) {
 
-  const i = props.value;
-  
-  return <button className="square" onClick={() => setClick({value: 'X'})}
-  >
-  {click}
-  </button>;
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Seat;
+
+// console.log("nextSeat[i]", nextSeat[i])
+// console.log('nextSeat', nextSeat)
--- src/pages/SelectSeats/index.js
+++ src/pages/SelectSeats/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Seat from "../../Components/Seat/Seat";
 
 export default function SelectSeats() {
+  const [seatValue, setSeatValue] = useState(Array(25).fill(null));
+
   function renderSeat(i) {
-    return <Seat value={i} />;
+    return (
+      <Seat
+        value={seatValue[i]}
+        onClick={() => {
+          const currentSeat = seatValue.slice();
+
+          if (currentSeat[i] === null) {
+            currentSeat[i] = "X";
+            setSeatValue(currentSeat);
+          } else {
+            currentSeat[i] = null;
+            setSeatValue(currentSeat);
+          }
+
+        }}
+      />
+    );
   }
 
   return (
-    <div>
+    <div className="seatStyle">
       <div className="board-row">
         {renderSeat(0)}
         {renderSeat(1)}
@@ -23,8 +41,31 @@ export default function SelectSeats() {
         {renderSeat(7)}
         {renderSeat(8)}
         {renderSeat(9)}
-        </div>
+      </div>
 
+      <div className="board-row">
+        {renderSeat(10)}
+        {renderSeat(11)}
+        {renderSeat(12)}
+        {renderSeat(13)}
+        {renderSeat(14)}
+      </div>
+
+      <div className="board-row">
+        {renderSeat(15)}
+        {renderSeat(16)}
+        {renderSeat(17)}
+        {renderSeat(18)}
+        {renderSeat(19)}
+      </div>
+
+      <div className="board-row">
+        {renderSeat(20)}
+        {renderSeat(21)}
+        {renderSeat(22)}
+        {renderSeat(23)}
+        {renderSeat(24)}
+      </div>
     </div>
   );
 }
