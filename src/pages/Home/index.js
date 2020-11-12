ef8abf69801b0353a0ce8ae0fa9fad06050ff151
Natalie Whitton <newhitton@gmail.com>
2020-11-12 21:11:19 +0100

    styling on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import MovieCarousel from "../../Components/Carousel/index"
+import MovieCarousel from "../../Components/Carousel/index";
+import "./style.css";
 
 export default function HomePage(props) {
   const { movies } = props;
   return (
     <>
+      <div className="homeBackground">
       <MovieCarousel movies={movies} />
+      </div>
     </>
   );
 }
