3e1688c38d8829d20851c5b39d4c0cb689b3e950
Natalie Whitton <newhitton@gmail.com>
2020-11-17 00:07:31 +0100

    commented out changes so I can deploy

--- src/pages/MovieDetails/index.js
+++ src/pages/MovieDetails/index.js
@@ -1,30 +1,30 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable array-callback-return */
-import React, { useContext } from "react";
-import AppContext from "../../store/context";
+// /* eslint-disable no-unused-expressions */
+// /* eslint-disable array-callback-return */
+// import React, { useContext } from "react";
+// import AppContext from "../../store/context";
 
-function MovieDetails(props) {
-  const { state } = useContext(AppContext);
-  const { movies } = props;
+// function MovieDetails(props) {
+//   const { state } = useContext(AppContext);
+//   const { movies } = props;
 
-  const aMovie = movies.filter(
-    (element) => element.movieTitle === state.ticket.filmTitle
-  );
+//   const aMovie = movies.filter(
+//     (element) => element.movieTitle === state.ticket.filmTitle
+//   );
 
-  const displayMovieSel = () => {
-    const disp = aMovie.map((movieInfo) => {
-       <div>{movieInfo.movieTitle}</div>;
-    });
+//   const displayMovieSel = () => {
+//     const disp = aMovie.map((movieInfo) => {
+//        <div>{movieInfo.movieTitle}</div>;
+//     });
     
-  };
+//   };
 
-  displayMovieSel();
+//   displayMovieSel();
 
-  console.log(aMovie);
+//   console.log(aMovie);
 
-  console.log("staaattteee from moviedetails", state.ticket);
-  return (
-    { displayMovieSel() });
-}
+//   console.log("staaattteee from moviedetails", state.ticket);
+//   return (
+//     { displayMovieSel() });
+// }
 
-export default MovieDetails;
+// export default MovieDetails;
