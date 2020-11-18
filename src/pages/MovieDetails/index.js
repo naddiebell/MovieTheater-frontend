aa4aa8b0faf17abec363d0f1c687f0cf051e7427
Natalie Whitton <newhitton@gmail.com>
2020-11-18 20:28:48 +0100

    film page styled.  Add ability to play youtube links with reactplayer.

--- src/pages/MovieDetails/index.js
+++ src/pages/MovieDetails/index.js
@@ -1,30 +1,66 @@
-// /* eslint-disable no-unused-expressions */
-// /* eslint-disable array-callback-return */
-// import React, { useContext } from "react";
-// import AppContext from "../../store/context";
+/* eslint-disable no-unused-expressions */
+/* eslint-disable array-callback-return */
+import React, { useContext } from "react";
+import ReactPlayer from "react-player";
+import AppContext from "../../store/context";
+import "./style.css";
 
-// function MovieDetails(props) {
-//   const { state } = useContext(AppContext);
-//   const { movies } = props;
+function MovieDetails(props) {
+  const { state } = useContext(AppContext);
+  const { movies } = props;
 
-//   const aMovie = movies.filter(
-//     (element) => element.movieTitle === state.ticket.filmTitle
-//   );
+  const aMovie = movies.filter(
+    (element) => element.movieTitle === state.ticket.filmTitle
+  );
 
-//   const displayMovieSel = () => {
-//     const disp = aMovie.map((movieInfo) => {
-//        <div>{movieInfo.movieTitle}</div>;
-//     });
+  const displayMovieSel = () => {
+    const disp = aMovie.map((movieInfo) => {
+      return (
+        <div className="background" key={movieInfo.id}>
+          <img
+            className="backgroundImgDiv"
+            src={movieInfo.backgroundImg}
+            alt={movieInfo.movieTitle}
+          />
+          <h1 className="movieTitle">{movieInfo.movieTitle}</h1>
+          <div className="movieDesc">
+            <ReactPlayer className="moviePoster" url={movieInfo.video} />
+
+            <div className="descAndInfo">
+              <p className="descriptionTitle">Beskrivning</p>
+              <p className="description">{movieInfo.description}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+    return disp;
+  };
+
+  const onClick = () => {
     
-//   };
+  }
 
-//   displayMovieSel();
+  return (
+    <>
+      {displayMovieSel()}
+      <button className="myButton ticketBtn" type="button">
+        Biljetter
+      </button>
+    </>
+  );
+}
 
-//   console.log(aMovie);
+export default MovieDetails;
 
-//   console.log("staaattteee from moviedetails", state.ticket);
-//   return (
-//     { displayMovieSel() });
-// }
+{
+  /* <img
+className="moviePoster"
+src={movieInfo.img}
+alt={movieInfo.img}
+/> */
+}
 
-// export default MovieDetails;
+/*
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Zn_qirpdBag" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+*/
