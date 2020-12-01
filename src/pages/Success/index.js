7a847c94184e19f153eb1b15a896adc6539cd242
Natalie Whitton <newhitton@gmail.com>
2020-12-01 11:24:58 +0100

    added emailjs email button function, error when sending

--- src/pages/Success/index.js
+++ src/pages/Success/index.js
@@ -1,52 +1,55 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import emailjs from "emailjs-com";
 import axios from "axios";
 
 const emailjsId = process.env.REACT_APP_EMAILJS_ID;
 const baseUrl = process.env.REACT_APP_BACKEND_URL;
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const emailJSApi = process.env.REACT_APP_EMAILJS_API;
+const templateId = process.env.REACT_APP_EJS_TEMPLATE_ID;
 
 function Success(props) {
   const { ticketId, children } = props;
-  console.log("ticketID", ticketId);
 
   const verifyPayment = async () => {
-    const repsonse = await axios.get(`${baseUrl}/api/v1/tickets/${ticketId}`, {
-      id: "Payment Successful",
-    });
-    return repsonse.data;
+    const res = await axios.put(
+      `${baseUrl}/api/v1/tickets/${ticketId}/validate`
+    );
+    return res.data;
   };
 
-  console.log("response", verifyPayment());
-  
-  function sendEmail(e) {
+  const sendEmail = async (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm("contact_service", "contact_form", e.target, emailjsId)
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  }
+    const ticketInfo = await verifyPayment();
+    const emailData = {
+      service_id: serviceId,
+      template_id: templateId,
+      user_id: emailjsId,
+      template_params: {
+        to_name: ticketInfo.userName,
+        user_email: ticketInfo.userEmail,
+        film: ticketInfo.filmName,
+        ticket_amount: ticketInfo.seatAmount,
+        date: ticketInfo.date,
+        price: ticketInfo.price,
+      },
+    };
+     await axios.post(emailJSApi, emailData);
+    console.log("I did it!");
+  };
 
   return (
-    <div>
+    <>
       {children}
-      <form className="contact-form" onSubmit={sendEmail}>
-        <input type="hidden" name="contact_number" />
-        <label>Name</label>
-        <input type="text" name="to_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <input type="submit" value="Send" />
-      </form>
-    </div>
+      <p>
+        Thank you for watching our favorite American class films with us! Your
+        payment was successful. You can click the button to send yourself your
+        receipt
+      </p>
+      <button className="myButton" type="button" onClick={sendEmail}>
+        Kvitto
+      </button>
+    </>
   );
 }
 
