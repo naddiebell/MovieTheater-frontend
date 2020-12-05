25332fb361ea0792f0d8c3ce3d0bf48b37a8f0c9
Natalie Whitton <newhitton@gmail.com>
2020-12-05 19:13:54 +0100

    added Link instead of a tag so the page wouldn't Flash when you clicked on the links in nav bar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable global-require */
 import React from "react";
+import { Link } from "@reach/router";
 import { useTranslation } from "react-i18next";
 import "./NavBar.css";
 
@@ -10,19 +11,19 @@ function NavBar() {
     <>
       <ul className="navBar">
         <li>
-          <a href="/">
+          <Link to="/">
             <img
               className="logoImage"
               alt="alttest"
               src={require("../../background_img/logo_transparent_crop.png")}
             />
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/biljetter"> {t("Tickets Button")}</a>
+          <Link to="/biljetter"> {t("Tickets Button")}</Link>
         </li>
         <li>
-          <a href="/om-oss">{t("About Us")}</a>
+          <Link to="/om-oss">{t("About Us")}</Link>
         </li>
         <button type="button" onClick={() => i18n.changeLanguage("en")}>Språk</button>
       </ul>
