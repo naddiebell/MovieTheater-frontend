e8f2c61d1c3f1d8dee735e255be6b32ad2860a4a
Natalie Whitton <newhitton@gmail.com>
2021-02-12 21:54:57 +0100

    styles to now playing text

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -46,8 +46,8 @@ function MovieCarousel(props) {
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
+      items: 5,
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
