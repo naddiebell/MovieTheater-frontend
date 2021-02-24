138ee688e74dba15c2662a08c07f9b073b226538
Natalie Whitton <newhitton@gmail.com>
2021-02-24 12:25:44 +0100

    commenting out email feature

--- src/pages/Success/index.js
+++ src/pages/Success/index.js
@@ -11,7 +11,7 @@ const templateId = process.env.REACT_APP_EJS_TEMPLATE_ID;
 
 function Success(props) {
   const { ticketId, children } = props;
-  const [disabledBtn, setDisableBtn] = useState(false);
+  // const [disabledBtn, setDisableBtn] = useState(false);
   const { t } = useTranslation();
 
   const verifyPayment = async () => {
@@ -21,25 +21,25 @@ function Success(props) {
     return res.data;
   };
 
-  const sendEmail = async () => {
-    const ticketInfo = await verifyPayment();
-    const adjustedPrice = ticketInfo.price / 100;
-    const emailData = {
-      service_id: serviceId,
-      template_id: templateId,
-      user_id: emailjsId,
-      template_params: {
-        to_name: ticketInfo.userName,
-        user_email: ticketInfo.userEmail,
-        film: ticketInfo.filmName,
-        ticket_amount: ticketInfo.seatAmount,
-        date: ticketInfo.date,
-        price: adjustedPrice,
-        seats: ticketInfo.seats,
-      },
-    };
-    await axios.post(emailJSApi, emailData);
-  };
+  // const sendEmail = async () => {
+  //   const ticketInfo = await verifyPayment();
+  //   const adjustedPrice = ticketInfo.price / 100;
+  //   const emailData = {
+  //     service_id: serviceId,
+  //     template_id: templateId,
+  //     user_id: emailjsId,
+  //     template_params: {
+  //       to_name: ticketInfo.userName,
+  //       user_email: ticketInfo.userEmail,
+  //       film: ticketInfo.filmName,
+  //       ticket_amount: ticketInfo.seatAmount,
+  //       date: ticketInfo.date,
+  //       price: adjustedPrice,
+  //       seats: ticketInfo.seats,
+  //     },
+  //   };
+  //   await axios.post(emailJSApi, emailData);
+  // };
 
   // const handleButton = (e) => {
   //   e.preventDefault();
