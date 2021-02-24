016ef96567f69c3bf359106aac806c69ab51e90b
Natalie Whitton <newhitton@gmail.com>
2021-02-24 11:52:34 +0100

    base for new component

---
+++ src/Components/MovieTimes/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function MovieCard() {
+
+    const movieCardFunc = (array) => {
+        const card = array.slice().map((aMovie) => {
+            return (
+                <div>
+                    
+                </div>
+            )
+        })
+    }
+
+  return card
+  <div>
+      
+  </div>;
+}
